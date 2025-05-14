4eeb5abad527aaee6775d3d867372d181f0b9ad3
Gregorio Vélez Román <greverom4@gmail.com>
2025-05-14 12:22:41 -0500

    agregado motion a header

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -1,11 +1,19 @@
+'use client'
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./mode-toggle";
+import { motion } from "framer-motion";
 
 export default function Header() {
   return (
     <header className="top-0 z-50 w-full bg-background transition-colors duration-300 ease">
-      <div className="mx-auto max-w-8xl w-full px-4 flex h-16 items-center justify-end gap-2">
+      <motion.div
+        initial={{ opacity: 0, y: -30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+        className="mx-auto max-w-8xl w-full px-4 flex h-16 items-center justify-end gap-2"
+      >
         <Link href="/auth/register">
           <Button
             className="px-3 py-2 text-sm sm:px-6 sm:py-2 sm:text-md"
@@ -23,7 +31,7 @@ export default function Header() {
           </Button>
         </Link> */}
         <ModeToggle />
-      </div>
+      </motion.div>
     </header>
   );
 }
